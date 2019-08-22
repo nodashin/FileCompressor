65482efee586de703b30594e3bf34223047c146b
野田慎吾 <rin.s.n.0314@Gmail.com>
2019-08-22 18:01:28 +0900

    コンプレッサー各クラスのコメント追加。

--- FileCompressor/FileCompressor/Compression/GZipCompressor.cs
+++ FileCompressor/FileCompressor/Compression/GZipCompressor.cs
@@ -6,12 +6,23 @@ using System.Threading.Tasks;
 
 namespace FileCompressor.Compression
 {
+    /// <summary>
+    /// GZipコンプレッサー
+    /// </summary>
     public class GZipCompressor : CompressorBase
     {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="outputDirectoryPath">圧縮ファイル出力先ディレクトリパス</param>
         public GZipCompressor(string outputDirectoryPath) : base(outputDirectoryPath, ".gz")
         {
         }
 
+        /// <summary>
+        /// 圧縮する。
+        /// </summary>
+        /// <param name="targetFilePaths">対象ファイルパス群</param>
         public override void Compress(List<string> targetFilePaths)
         {
             throw new NotImplementedException();
--- FileCompressor/FileCompressor/Compression/ZipCompressor.cs
+++ FileCompressor/FileCompressor/Compression/ZipCompressor.cs
@@ -11,10 +11,18 @@ namespace FileCompressor.Compression
     /// </summary>
     public class ZipCompressor : CompressorBase
     {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="outputDirectoryPath">圧縮ファイル出力先ディレクトリパス</param>
         public ZipCompressor(string outputDirectoryPath) : base(outputDirectoryPath, ".zip")
         {
         }
 
+        /// <summary>
+        /// 圧縮する。
+        /// </summary>
+        /// <param name="targetFilePaths">対象ファイルパス群</param>
         public override void Compress(List<string> targetFilePaths)
         {
             throw new NotImplementedException();
